9e675d1b39770a72364f5e26d96f5ab1b1af8f2d
akakayka <pre_first@mail.ru>
2025-01-08 02:14:09 +0500

    example-block

---
+++ assets/scripts/before-after.js
@@ -0,0 +1,43 @@
+function imageComparison(selector) {
+
+    let comparison = $(selector)
+        .addClass("image-comparison")
+        .prepend('<div class="image-comparison__before"></div>')
+        .append('<button class="image-comparison__slider"></button>');
+
+    let images = comparison
+        .find("img")
+        .addClass("image-comparison__image")
+        .css("max-width", comparison.width());
+
+    let before = comparison
+        .find(".image-comparison__before")
+        .append(images.eq(0));
+
+    comparison
+        .find(".image-comparison__slider")
+        .on("dragstart", () => false)
+        .on("mousedown", function(e) {
+            let slider = $(this);
+            let doc = $(document).on("mousemove", (e) => {
+                let offset = e.pageX - comparison.position().left;
+                let content_width = comparison.width();
+                let el = document.getElementsByClassName("ba-block")[0]
+                let width = el.offsetWidth
+                let dif = ((width - content_width)/2)-5
+                
+
+                if (offset < -dif) offset = -dif;
+                if (offset > content_width+dif) {
+                    console.log(width)
+                    offset = content_width+dif;
+                }
+                slider.css("left", offset + "px");
+                before.css("width", offset + "px");
+            });
+
+            doc.on("mouseup", () => doc.off("mousemove"));
+        });
+};
+
+imageComparison("#image-comparison");
---
+++ assets/scripts/slider.js
@@ -0,0 +1,71 @@
+/*let firstSlideIndex = 1
+let secondSlideIndex = 2
+let count = document.getElementsByClassName("examples-card__content-right__item").length
+
+showSlides(firstSlideIndex, secondSlideIndex)
+
+function nextSlide() {
+    firstSlideIndex = secondSlideIndex
+    secondSlideIndex += 1
+    if(secondSlideIndex > count)
+    {
+        secondSlideIndex = 1
+    }
+    showSlides(firstSlideIndex, secondSlideIndex);
+}
+
+function previousSlide() {
+    secondSlideIndex = firstSlideIndex
+    firstSlideIndex -= 1
+    if(firstSlideIndex < 1)
+    {
+        firstSlideIndex = count
+    }
+    showSlides(firstSlideIndex, secondSlideIndex);  
+}
+
+function showSlides(n, m) {
+
+    let slides = document.getElementsByClassName("examples-card__content-right__item");
+
+    for (let slide of slides) {
+        slide.style.display = "none";
+    }
+
+    slides[n - 1].style.display = "flex";
+    slides[m - 1].style.display = "flex";    
+}*/
+
+let firstSlideIndex = 1
+let count = document.getElementsByClassName("examples-card__content-right__item").length
+
+showSlides(firstSlideIndex)
+
+function nextSlide() {
+    firstSlideIndex += 1
+    if(firstSlideIndex > count)
+    {
+        firstSlideIndex = 1
+    }
+    showSlides(firstSlideIndex);
+}
+
+function previousSlide() {
+    firstSlideIndex -= 1
+    if(firstSlideIndex < 1)
+    {
+        firstSlideIndex = count
+    }
+    showSlides(firstSlideIndex);  
+}
+
+function showSlides(n) {
+
+    let slides = document.getElementsByClassName("examples-card__content-right__item");
+
+    for (let slide of slides) {
+        slide.style.display = "none";
+    }
+
+    slides[n - 1].style.display = "flex";
+}
