b9691689c8761bbece6f8576cf74ccc8327e6673
Maxim Mineev <mms1401@ya.ru>
2025-01-10 16:57:49 +0500

    add examples

--- assets/scripts/slider.js
+++ assets/scripts/slider.js
@@ -1,40 +1,40 @@
-/*let firstSlideIndex = 1
-let secondSlideIndex = 2
-let count = document.getElementsByClassName("examples-card__content-right__item").length
+// let firstSlideIndex = 1
+// let secondSlideIndex = 2
+// let count = document.getElementsByClassName("examples-card__content-right__item").length
 
-showSlides(firstSlideIndex, secondSlideIndex)
+// showSlides(firstSlideIndex, secondSlideIndex)
 
-function nextSlide() {
-    firstSlideIndex = secondSlideIndex
-    secondSlideIndex += 1
-    if(secondSlideIndex > count)
-    {
-        secondSlideIndex = 1
-    }
-    showSlides(firstSlideIndex, secondSlideIndex);
-}
+// function nextSlide() {
+//     firstSlideIndex = secondSlideIndex
+//     secondSlideIndex += 1
+//     if(secondSlideIndex > count)
+//     {
+//         secondSlideIndex = 1
+//     }
+//     showSlides(firstSlideIndex, secondSlideIndex);
+// }
 
-function previousSlide() {
-    secondSlideIndex = firstSlideIndex
-    firstSlideIndex -= 1
-    if(firstSlideIndex < 1)
-    {
-        firstSlideIndex = count
-    }
-    showSlides(firstSlideIndex, secondSlideIndex);  
-}
+// function previousSlide() {
+//     secondSlideIndex = firstSlideIndex
+//     firstSlideIndex -= 1
+//     if(firstSlideIndex < 1)
+//     {
+//         firstSlideIndex = count
+//     }
+//     showSlides(firstSlideIndex, secondSlideIndex);  
+// }
 
-function showSlides(n, m) {
+// function showSlides(n, m) {
 
-    let slides = document.getElementsByClassName("examples-card__content-right__item");
+//     let slides = document.getElementsByClassName("examples-card__content-right__item");
 
-    for (let slide of slides) {
-        slide.style.display = "none";
-    }
+//     for (let slide of slides) {
+//         slide.style.display = "none";
+//     }
 
-    slides[n - 1].style.display = "flex";
-    slides[m - 1].style.display = "flex";    
-}*/
+//     slides[n - 1].style.display = "flex";
+//     slides[m - 1].style.display = "flex";    
+// }
 
 let firstSlideIndex = 1;
 let count = document.getElementsByClassName(
