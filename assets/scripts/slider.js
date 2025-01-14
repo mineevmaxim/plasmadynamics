63803aa2de77599f09682a33b9e2ac683f299b74
akakayka <pre_first@mail.ru>
2025-01-14 13:08:01 +0500

    fix

--- assets/scripts/slider.js
+++ assets/scripts/slider.js
@@ -1,59 +1,72 @@
+// let firstSlideIndex = 1
+// let secondSlideIndex = 2
+// let count = document.getElementsByClassName("examples-card__content-right__item").length
+
+// showSlides(firstSlideIndex, secondSlideIndex)
+
+// function nextSlide() {
+//     firstSlideIndex = secondSlideIndex
+//     secondSlideIndex += 1
+//     if(secondSlideIndex > count)
+//     {
+//         secondSlideIndex = 1
+//     }
+//     showSlides(firstSlideIndex, secondSlideIndex);
+// }
+
+// function previousSlide() {
+//     secondSlideIndex = firstSlideIndex
+//     firstSlideIndex -= 1
+//     if(firstSlideIndex < 1)
+//     {
+//         firstSlideIndex = count
+//     }
+//     showSlides(firstSlideIndex, secondSlideIndex);  
+// }
+
+// function showSlides(n, m) {
+
+//     let slides = document.getElementsByClassName("examples-card__content-right__item");
+
+//     for (let slide of slides) {
+//         slide.style.display = "none";
+//     }
+
+//     slides[n - 1].style.display = "flex";
+//     slides[m - 1].style.display = "flex";    
+// }
+
 let firstSlideIndex = 1;
-let count = document.getElementsByClassName("examples-card__content-right__item").length;
-let startX; // Для отслеживания начальной позиции касания
+let count = document.getElementsByClassName(
+    "examples-card__content-right__item"
+).length;
 
 showSlides(firstSlideIndex);
 
-// Функции переключения слайдов
 function nextSlide() {
-    firstSlideIndex += 1;
-    if (firstSlideIndex > count) {
-        firstSlideIndex = 1;
-    }
-    showSlides(firstSlideIndex);
+    firstSlideIndex += 1;
+    if (firstSlideIndex > count) {
+        firstSlideIndex = 1;
+    }
+    showSlides(firstSlideIndex);
 }
 
 function previousSlide() {
-    firstSlideIndex -= 1;
-    if (firstSlideIndex < 1) {
-        firstSlideIndex = count;
-    }
-    showSlides(firstSlideIndex);
+    firstSlideIndex -= 1;
+    if (firstSlideIndex < 1) {
+        firstSlideIndex = count;
+    }
+    showSlides(firstSlideIndex);
 }
 
-// Функция отображения слайдов
 function showSlides(n) {
-    let slides = document.getElementsByClassName("examples-card__content-right__item");
-    for (let slide of slides) {
-        slide.style.display = "none";
-    }
-    slides[n - 1].style.display = "flex";
-}
+    let slides = document.getElementsByClassName(
+        "examples-card__content-right__item"
+    );
 
-// Обработка жестов касания
-const sliderContainer = document.querySelector('.examples-card__content-right'); // Замените на ваш контейнер слайдера
+    for (let slide of slides) {
+        slide.style.display = "none";
+    }
 
-sliderContainer.addEventListener('touchstart', handleTouchStart, false);
-sliderContainer.addEventListener('touchmove', handleTouchMove, false);
-
-function handleTouchStart(evt) {
-    const firstTouch = evt.touches[0];
-    startX = firstTouch.clientX; // Получаем начальную позицию по оси X
-}
-
-function handleTouchMove(evt) {
-    if (!startX) return; // Если не получена начальная позиция, завершить функцию
-
-    let x = evt.touches[0].clientX; // Получаем позицию касания по оси X
-    let xDiff = startX - x; // Разница между начальной и текущей позицией
-
-    if (Math.abs(xDiff) > 30) { // Если разница больше 50 пикселей
-        if (xDiff > 0) {
-            nextSlide(); // Свайп влево, след. слайд
-        } else {
-            previousSlide(); // Свайп вправо, предыдущий слайд
-        }
-    }
-
-    startX = null; // Сбрасываем начальную позицию
+    slides[n - 1].style.display = "flex";
 }
